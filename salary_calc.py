f66b1997e0e1678b61c11010461a01d161f89f8c
Joseph-R <Semisonic8100@gmail.com>
2014-03-25 07:20:21 -0400

    Updating salary_calc.py

--- salary_calc.py
+++ salary_calc.py
@@ -1,8 +1,12 @@
-salary = int(raw_input('Enter Salary: '))
+#  Rough tool to gauge annual, monthly, and weekly take-home pay after taxes based on projected total tax burden.  
+#  See http://usatoday30.usatoday.com/news/washington/tax-rates-spending.htm for a more comprehensive breakdown.
 
-salary = salary * .75  ## Assumes 25% tax bracket
+salary = float(raw_input('Enter Salary: '))
+tax_burden = float(raw_input('Enter tax burden [Ex 25% = .25]: '))
+
+salary = salary * (1 - tax_burden)  
 annual = salary
 monthly = salary / 12
 weekly = salary / 52
 
-print "Annual: %r, Monthly %r, Weekly %r." % (annual, monthly, weekly)
+print "Annual: $%r,\nMonthly $%r,\nWeekly $%r." % (annual, monthly, weekly)
